fe31a1a12af58db6e363d81145b0349e8d360df1
gloomydumber <chromacrow@naver.com>
2020-03-04 15:56:50 +0900

    bookstudypatch

--- kakaotalk.js
+++ kakaotalk.js
@@ -41,4 +41,31 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
   if (msg == "오늘") {
     replier.reply("주인님, 현재시간 알려드립니다.\n" + today);
   }
+  if (msg == "/실검") {
+    var data =
+      org.jsoup.Jsoup.connect(
+        "https://m.search.naver.com/search.naver?query=실시간%20검색어"
+      )
+        .get()
+        .select("span.tit") + "";
+    data = data.replace(/<[^>]+>/g, "");
+    data = data.split("\n");
+    var result = "";
+    for (var n = 0; n < 20; n++) {
+      result += n + 1 + "위: " + data[n] + "\n";
+    }
+    replier.reply("[네이버 실시간 검색어 순위]\n\n" + result.trim());
+  }
+
+  // from here 내가 만든 kospi
+  var myURL =
+    "https://api.worldtradingdata.com/api/v1/stock?symbol=KOSPI.KS&api_token=mkgXnjQlBeDdpzHXsL5nWO7BUD0qVjRrW7onDWO9PcRGZNiE4kqT9Vah9FC1";
+
+  var data =
+    org.jsoup.Jsoup.connect(myURL)
+      .ignoreContentType(true)
+      .get()
+      .select("body") + " ";
+
+  var realprice = data.substr(122, 7); //114 7
 }
---
+++ outjs.js
@@ -0,0 +1,6 @@
+var reword = prompt("type 5times word", "");
+var i = 0;
+while (i < 5) {
+  document.write(reword + "<br/>");
+  i++;
+}
